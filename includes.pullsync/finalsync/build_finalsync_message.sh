1923296f197cf9833bedddbcccd2b26f3e2812e2
Andrej Walilko <awalilko@liquidweb.com>
2024-03-20 09:38:01 -0400

    resolve build_finalsync_message

--- includes.pullsync/finalsync/build_finalsync_message.sh
+++ includes.pullsync/finalsync/build_finalsync_message.sh
@@ -1,12 +1,10 @@
 build_finalsync_message() { #create a ticket reply for post-finalsync based on server and nameserver information, including sync-specific information
 	local cmd=(dialog --nocancel --colors --clear --backtitle "pullsync" --title "Build Final Sync Message" --radiolist "Select a nameserver hosting option for building the post-final message to the client.\n" 0 0 7)
 	local options=( 1 "Internal to source (and customer will reuse same nameservers)" off
-			2 "Internal to source, but registered by LiquidWeb" off
-			3 "Customer will use brand new nameservers on new server" off
-			4 "ns.liquidweb.com" off
-			5 "External to source (e.g. GoDaddy, Cloudflare, etc.)" off
-			6 "Other (add your own information when copy/pasting)" off
-			7 "IP swap, doesn't matter" off)
+			2 "Customer will use brand new nameservers on new server" off
+			3 "External to source (e.g. GoDaddy, Cloudflare, etc.)" off
+			4 "Other (add your own information when copy/pasting)" off
+			5 "IP swap, doesn't matter" off)
 
 	ec yellow "Building finalsync reply..."
 	sleep 1 #make it look like we are working
@@ -33,45 +31,30 @@ build_finalsync_message() { #create a ticket reply for post-finalsync based on s
 	#use the nameserver registrar and contact information to guide suggestion
 	local registrar=$(nameserver_registrar $nameserver1)
 	[ "$registrar" = "" ] && unset registrar #bail if not detected
-	if [ "$registrar" = "ENOM, INC." ]; then
-		techcontact=$(whois $(echo $nameserver1 | cut -d. -f2-) | awk -F: '/^Tech Organization:/ {print $NF}' | tr -d ' ')
-		if echo $techcontact | grep -q -i liquidweb; then
-			cmd[9]=$(echo "${cmd[9]}\n\Z1${nameserver1}'s main domain seems to be registered by liquidweb!\nPlease remember to update this at the conclusion of the DNS changes if needed!\Zn\n")
-		elif echo $techcontact | grep -q -i whoisguard; then
-			cmd[9]=$(echo "${cmd[9]}\n\Z1${nameserver1}'s main domain seems to be registered at enom, but whoisguard is on.\nThis could be registered by liqudweb! double check in billing before making a selection!\Zn\n")
-		else
-			cmd[9]=$(echo "${cmd[9]}\n${nameserver1}'s main domain seems to be registered at $registrar.\n")
-		fi
-	elif [ ! "$registrar" = "" ]; then
+	if [ ! "$registrar" = "" ]; then
 		cmd[9]=$(echo "${cmd[9]}\n${nameserver1}'s main domain seems to be registered at $registrar.\n")
 	fi
 
 	#create suggestion
-	# per #139, disabled ip swap temporarily, but added "dummyipswap" variable to suggest ip swap verbiage.
-	if [ "$ipswap" -o "$stormipswap" -o "$dummyipswap" ]; then
-		options[20]=on
-		cmd[9]=$(echo "${cmd[9]}\nI recommend picking option 7, ipswap variable is set.")
+	if [ "$ipswap" ]; then
+		options[14]=on
+		cmd[9]=$(echo "${cmd[9]}\nI recommend picking option 5, ipswap variable is set.")
 	elif head -2 $dir/nameserver_summary.txt | grep -q $nameserver1 && head -2 $dir/nameserver_summary.txt | grep -q $nameserver2; then
 		if [ ! "$copydns" ]; then
-			options[14]=on
-			cmd[9]=$(echo "${cmd[9]}\nI recommend picking option 5, top nameservers are those set on this machine, but you elected to not copy DNS.")
-		elif echo $techcontact | grep -q -i -e liquidweb -e whoisguard; then
-			options[5]=on
-			cmd[9]=$(echo "${cmd[9]}\nI recommend picking option 2, nameservers from /etc/wwwacct.conf match top two used nameservers, registration possibly by liquidweb.")
+			options[8]=on
+			cmd[9]=$(echo "${cmd[9]}\nI recommend picking option 3, top nameservers are those set on this machine, but you elected to not copy DNS.")
 		else
 			options[2]=on
 			cmd[9]=$(echo "${cmd[9]}\nI recommend picking option 1, nameservers from /etc/wwwacct.conf match top two used nameservers.")
 		fi
-	elif head -2 $dir/nameserver_summary.txt | grep -q -e liquidweb.com -e sourcedns.com -e wiredtree.com; then
-		options[11]=on
-		cmd[9]=$(echo "${cmd[9]}\nI recommend picking option 4, top used nameservers are liquidweb, wiredtree, or sourcedns.")
 	elif ! head -2 $dir/nameserver_summary.txt | grep -q -e $nameserver1 -e $nameserver2 -e $snameserver1 -e $snameserver2; then
-		options[14]=on
-		cmd[9]=$(echo "${cmd[9]}\nI recommend picking option 5, top used nameservers are neither source or target's.")
-	elif ! grep ^NS $dir/etc/wwwacct.conf | grep -q -e $nameserver1 -e $nameserver2; then
 		options[8]=on
-		cmd[9]=$(echo "${cmd[9]}\nI recommend picking option 3, nameservers do not match between source and target.")
+		cmd[9]=$(echo "${cmd[9]}\nI recommend picking option 3, top used nameservers are neither source or target's.")
+	elif ! grep ^NS $dir/etc/wwwacct.conf | grep -q -e $nameserver1 -e $nameserver2; then
+		options[5]=on
+		cmd[9]=$(echo "${cmd[9]}\nI recommend picking option 2, nameservers do not match between source and target.")
 	else
+		options[11]=on
 		cmd[9]=$(echo "${cmd[9]}\n\Z1I couldn't logic out what to pick... hope you know what to choose!\Zn")
 	fi
 
@@ -104,14 +87,6 @@ EOF
 			fi
 			;;
 			2) cat >> $dir/finalsyncreply.txt << EOF
-The final sync is complete, and the DNS files for the domains have been synced back to the original server. We have also updated the nameserver IPs for you at the domain registrar, since your nameservers' primary domain was registered through us. You can see that the updates are public and propagated by using this tool:
-
-https://www.whatsmydns.net/#A/$testdomain
-
-DNS for any domains that do not depend on your internal nameservers should be updated at this time. With this, the migration can be considered fully complete.
-EOF
-			;;
-			3) cat >> $dir/finalsyncreply.txt << EOF
 The final sync is complete, and it is now time to update the DNS for your domains to the new nameservers, ${nameserver1} and ${nameserver2}. The nameservers for the domains are updated at their respective registrars. Please also ensure that the nameservers themselves are registered at their main domain's registrar. You can use the following IPs for each nameserver:
 
 ${nameserver1:-ns1} - $first_ip
@@ -128,16 +103,7 @@ This is done at the domain registrar for the main domain. Let me know if you hav
 EOF
 			fi
 			;;
-			4) cat >> $dir/finalsyncreply.txt << EOF
-The final sync is complete, and the DNS for the domains has been updated on our nameservers, ns.liquidweb.com. This has made the sites live on the new server. You can see that the updates are public and propagated by using this tool:
-
-https://www.whatsmydns.net/#A/$testdomain
-
-DNS for any domains that do not depend on our Liquid Web nameservers should be updated at this time. With this, the migration can be considered fully complete.
-
-EOF
-			;;
-			5) cat >> $dir/finalsyncreply.txt << EOF
+			3) cat >> $dir/finalsyncreply.txt << EOF
 The final sync is complete. Now is the time to update DNS for your domains to the new IPs. This is done at the current nameservers for each migrated domain. Once you make these changes, you can check and see that the updates are public and propagated by using this tool:
 
 https://www.whatsmydns.net/#A/$testdomain
@@ -146,10 +112,10 @@ Let us know if you have any questions on this task. Once you update the DNS, the
 
 EOF
 			;;
-			6) echo -e "[REPLACE ME WITH DNS DETAILS]\n[REPLACE ME WITH DNS DETAILS]\n[REPLACE ME WITH DNS DETAILS]\n" >> $dir/finalsyncreply.txt
+			4) echo -e "[REPLACE ME WITH DNS DETAILS]\n[REPLACE ME WITH DNS DETAILS]\n[REPLACE ME WITH DNS DETAILS]\n" >> $dir/finalsyncreply.txt
 			cue_tech_to_replace_dns_details=1
 			;;
-			7) cat >> $dir/finalsyncreply.txt << EOF
+			5) cat >> $dir/finalsyncreply.txt << EOF
 The final sync is finished, and the IP addresses for your server have been moved to the new machine and assigned to their respective original accounts. This has made the sites live on the new server, and the migration can now be considered fully complete.
 
 EOF
@@ -160,9 +126,9 @@ EOF
 
 	#always print this part
 	cat >> $dir/finalsyncreply.txt << EOF
-If you made edits to your workstation's hosts file to test the sites during the course of the migration, these changes should be reverted at this time, so that your computer will start using public DNS records for your domains again. Leaving these hosts file records in place can cause issues with domain resolution in the future. If you need assistance editing your hosts file, please review the following link or call our support team:
+If you made edits to your workstation's hosts file to test the sites during the course of the migration, these changes should be reverted at this time, so that your computer will start using public DNS records for your domains again. Leaving these hosts file records in place can cause issues with domain resolution in the future. If you need assistance editing your hosts file, please review the following link:
 
-https://www.liquidweb.com/kb/dns-hosts-file/
+https://www.howtogeek.com/howto/27350/beginner-geek-how-to-edit-your-hosts-file/
 
 EOF
 
@@ -172,7 +138,6 @@ EOF
 Now is a good time to ensure that you have appropriate backups set up and enabled for your data. I show that cPanel backups are enabled on the new server, though you may wish to investigate the retention schedule. Server-side backups can be enabled and adjusted through WHM, and we also offer remote backup solutions for all server types. More details can be found at these links, and we are happy to go over the options available to you.
 
 https://www.liquidweb.com/kb/enable-cpanel-backups-in-whm/
-https://www.liquidweb.com/products/add-ons/storage-backups/acronis-cyber-backups/
 
 EOF
 	else
@@ -180,33 +145,13 @@ EOF
 Now is a good time to ensure that you have appropriate backups set up and enabled for your data, and I show that presently, cPanel backups are DISABLED on the target server. Server-side backups can be enabled through WHM, and we also offer remote backup solutions for all server types. More details can be found at these links, and we are happy to go over the options available to you.
 
 https://www.liquidweb.com/kb/enable-cpanel-backups-in-whm/
-https://www.liquidweb.com/products/add-ons/storage-backups/acronis-cyber-backups/
 
 EOF
 	fi
 
-	# detect if lw source, and if so storm/dedi; give termination strategy
-	if [ -f $dir/usr/local/lp/etc/lp-UID ]; then
-		if sssh "df | awk '{print $1}'" | grep -qE 'vda[0-9]'; then
-			cat >> $dir/finalsyncreply.txt << EOF
-Keep in mind that we will not automatically cancel the old hosting solution for you; this must be requested in a separate ticket once you are sure you no longer need data from that machine. A backup archive of the server is recommended in cases when long term retention may be needed, which could be accomplished through a Storm Image:
-
-https://www.liquidweb.com/kb/creating-storm-server-images/
-
-As always, please let us know if you have any questions or encounter any issues with the new server.
-EOF
-		else
-			cat >> $dir/finalsyncreply.txt << EOF
-Keep in mind that we will not automatically cancel the old hosting solution for you; this must be requested in a separate ticket once you are sure you no longer need data from that machine.
-
-As always, please let us know if you have any questions or encounter any issues with the new server.
-EOF
-		fi
-	else
-		cat >> $dir/finalsyncreply.txt << EOF
+	cat >> $dir/finalsyncreply.txt << EOF
 Keep in mind that we will not automatically cancel the old hosting solution for you; this must be done with the old hosting company separately following the update of DNS.
 
 As always, please let us know if you have any questions or encounter any issues with the new server.
 EOF
-	fi
 }
