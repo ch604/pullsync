1e802b6bc1edda64beea7dd9d45db9a6ab3a84c9
Andrej Walilko <awalilko@liquidweb.com>
2025-03-06 20:10:58 -0500

    move files to parent directory

--- includes.pullsync/ec.sh
+++ includes.pullsync/ec.sh
@@ -1,7 +1,6 @@
 ec() { #color printing
 	# get the color and shift arguments
-	local ecolor=${!1}
-	shift
+	local ecolor=${!1}; shift
 	# print remaining args, change back to no color, and log the line
-	echo -e ${ecolor}"${*}"${nocolor} | logit
+	echo -e "${ecolor}${*}${nocolor}" | logit
 }
